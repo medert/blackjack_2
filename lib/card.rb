815b5237e734270c9950e8df1f148c09f0741c0e
Meder <m.toktosunov@icloud.com>
2016-10-22 18:21:18 -0400

    first commit

---
+++ lib/card.rb
@@ -0,0 +1,8 @@
+class Card
+  attr_reader :suit, :value
+
+  def initialize()
+    @suit = suite
+    @value = value
+  end
+end
---
+++ spec/lib/card_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+RSpec.describe Card do
+  it "initialize" do
+    card = Card.new(5, ♣)
+  end
+end
